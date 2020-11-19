ddee0b847a2813ec71af4b79040f82067dd9a99d
Victoria Noci <48250726+VictoriaNoci@users.noreply.github.com>
2020-11-19 11:35:13 +0100

    update loginpage

--- loginpage.py
+++ loginpage.py
@@ -67,7 +67,7 @@ def success():
     n_usuarios= es.search(index='usuarios')  # Obtener nº usuarios para añadir 1 más
     n_usuarios = n_usuarios['hits']['total'] + 1  # ID + 1
 
-    # Insercción en BBDD local
+    # Inserción en BBDD local
     nombre_usuario= es.search(index='usuarios',body={'query':{'match':{'username':session['user']}}})      # POR API
     if (nombre_usuario['hits']['total'] == 0):      # No hay nadie con ese nombre de usuario
         email_usuario= requests.get('http://localhost:9200/usuarios/data/_search?q=email:"'+ session['email'] + '"&pretty')     # POR URL
@@ -85,19 +85,21 @@ def success():
 @app.route('/login')
 def login():
     if 'email' in session:
-        return render_template('index.html',msj=session['user'] + " está conectado",peticiones='Peticiones de media BBDD local: '+ str(session['peticiones']))
+        return render_template('index.html',msj=session['email'] + " está conectado",peticiones='Peticiones de media BBDD local: '+ str(session['peticiones']))
     else:
         return render_template("login.html")
 @app.route('/success_login',methods = ["POST"])
 def success_login():
-    session['email']=request.form['email']
-    session['pass']=request.form['pass']
+    email=request.form['email']
+    password_introducida=request.form['pass']
 
     # Comprobar que existe en la BBDD
-    email_usuario= requests.get('http://localhost:9200/usuarios/data/_search?q=email:"'+ session['email'] + '"&pretty')     # POR URL
-    if (email_usuario.json()['hits']['total'] == 1):      # Hemos encontrado su email
+    email_usuario= requests.get('http://localhost:9200/usuarios/data/_search?q=email:"'+ email + '"&pretty')     # POR URL
+    if (email_usuario.json()['hits']['total'] == 1):      # Hemos encontrado su email (Usuario existe)
         session['user']= email_usuario.json()['hits']['hits'][0]['_source']['username']   # Añadimos a variables de sesion el nombre de usuario
+        session['email']= email
         session['peticiones']= email_usuario.json()['hits']['hits'][0]['_source']['peticiones']   # Añadimos a variables de sesión el número de peticiones
+        session['pass'] = password_introducida
 
         pass_hasheada = email_usuario.json()['hits']['hits'][0]['_source']['password'] # Comprobamos que la contraseña es correcta
         password, salt = pass_hasheada.split(':')
@@ -108,7 +110,7 @@ def success_login():
         else:
             return render_template('login.html',msj="La contraseña introducida no es correcta.")
     else:
-        return render_template('login.html',msj="Email: " + session['email'] + " no existe en nuestra BBDD. ¡Regístrate primero!")
+        return render_template('login.html',msj="Email: " + email + " no existe en nuestra BBDD. ¡Regístrate primero!")
 
 @app.route('/logout')
 def logout():
@@ -121,9 +123,10 @@ def logout():
 
 @app.route('/media_1',methods = ["POST"])
 def media_1():   # Solo disponible para usuarios registrados y +1 peticiones dentro ficha BBDD
-    if 'user' in session:      # Hay una sesión iniciada
+    if 'email' in session:      # Hay una sesión iniciada
         n_aleatorios= es.search(index='aleatorios')  # Obtener nº de números aleatorios en la bbdd
         n_aleatorios = n_aleatorios['hits']['total']
+        print('N elementos LOCAL: ' + str(n_aleatorios))
         media = 0
         for i in range(n_aleatorios):
             numero= requests.get('http://localhost:9200/aleatorios/data/'+ str(i+1) +'?pretty')
@@ -137,26 +140,27 @@ def media_1():   # Solo disponible para usuarios registrados y +1 peticiones den
         session['peticiones'] = datos_usuario['peticiones']
         respuesta = es.index(index='usuarios',doc_type='data',id=usuario['hits']['hits'][0]['_id'],body=datos_usuario)
         if (respuesta['result'] == 'updated'):
-            return render_template('index.html',msj=session['user'] + " está conectado",peticiones='Peticiones de media BBDD local: '+ str(session['peticiones']),media1="Media BBDD local: " + str(media))
+            return render_template('index.html',msj=session['user'] + " está conectado",peticiones='Peticiones de media BBDD local: '+ str(session['peticiones']),media="Media BBDD local: " + str(media))
     else:
         return render_template('index.html',msj="Media solo disponible para usuarios registrados")
 
 @app.route('/media_2',methods = ["POST"])    # BBDD internet
 def media_2():
-    if 'user' in session:      # Hay una sesión iniciada
+    if 'email' in session:      # Hay una sesión iniciada
         lista_aleatorios = requests.get('https://api.thingspeak.com/channels/1194117/feeds.json?api_key=ER2RW1ORI6791BT2')
         n_datos = lista_aleatorios.json()['channel']['last_entry_id']
+        print('N elementos INTERNET: ' + str(n_datos))
         media = 0
         for i in range(n_datos):
             media = media + int(lista_aleatorios.json()['feeds'][i]['field1'])
         media = media/n_datos
-        return render_template('index.html',msj=session['user'] + " está conectado",peticiones='Peticiones de media BBDD local: '+ str(session['peticiones']),media2="Media BBDD internet: " + str(media))
+        return render_template('index.html',msj=session['user'] + " está conectado",peticiones='Peticiones de media BBDD local: '+ str(session['peticiones']),media="Media BBDD internet: " + str(media))
     else:
         return render_template('index.html',msj="Media solo disponible para usuarios registrados")
 
 @app.route('/umbral_historico',methods = ["POST"])
 def umbral_historico():    # 5 últimos valores en los que se superó umbral
-    if 'user' in session:      # Hay una sesión iniciada
+    if 'email' in session:      # Hay una sesión iniciada
         umbral=request.form['umbral']
         lista_aleatorios = requests.get('https://api.thingspeak.com/channels/1194117/feeds.json?api_key=ER2RW1ORI6791BT2')
         n_datos = int(lista_aleatorios.json()['channel']['last_entry_id'])
